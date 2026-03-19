2c7e20e282e879ae98958d14a592c75e37448d8c
evely.soares <terezaevely@gmail.com>
2026-03-19 15:04:39 -0300

    KeyWords examples created

---
+++ src/fundamentals/keywords/KeyWords.java
@@ -0,0 +1,37 @@
+package fundamentals.keywords;
+
+public class KeyWords {
+    /*
+    A set of reserved keywords that cannot be used as variables, methods, classes, or any other identifiers.
+
+    Examples below.
+    */
+
+    public static void keyWordsExamples(){
+        System.out.println(" -- Key Words Examples --");
+
+        System.out.println("♨ Primitive data types: \n" +
+                "byte, int, short, long (Integer numbers)\n" +
+                "float, double (float numbers)\n" +
+                "char (char type)\n" +
+                "boolean ( true or false)\n");
+
+        System.out.println("♨ Flow control: \n" +
+                "if, else (conditional structures)\n" +
+                "switch, case, default (multiple conditional selection)\n" +
+                "while, do, for (repetition loops)\n" +
+                "break (interrupt instructions)\n" +
+                "return (Finished the execution of a method, and can be used to return a value from a method)\n");
+        System.out.println("♨ Object and Exception Handling:\n" +
+                "new  (Creates a new object instance)\n" +
+                "this (Refers to the current instance)\n" +
+                "super (Refers to the superclass)\n" +
+                "instanceof (Checks the object's type)\n" +
+                "try, catch, finally (Approach handling blocks)\n" +
+                "throw (Throws an exception)\n" +
+                "throws (Declares approaches that the method can throw)");
+    }
+
+
+
+}
