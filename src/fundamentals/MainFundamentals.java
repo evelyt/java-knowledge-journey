47b38626230e510874d21731f697fa01245364d4
evely.soares <terezaevely@gmail.com>
2026-03-19 16:59:21 -0300

    refactor main

--- src/fundamentals/MainFundamentals.java
+++ src/fundamentals/MainFundamentals.java
@@ -1,15 +1,16 @@
+package fundamentals;
+
 import static fundamentals.keywords.KeyWords.keyWordsExamples;
 import static fundamentals.operators.ArithmeticOperators.arithmeticOperators;
 import static fundamentals.operators.AssignmentOperators.assignmentOperators;
 import static fundamentals.operators.LogicalOperators.logicalOperators;
 
-public class Main {
+public class MainFundamentals {
     public static void main(String[] args) {
         //Practice
         keyWordsExamples();
         assignmentOperators();
         arithmeticOperators();
         logicalOperators();
-        //exercises
     }
 }
