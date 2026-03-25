e3d9c03386a9f80aaac1df5e41d80d5a37163ff4
evelyt <terezaevely@gmail.com>
2026-03-24 23:06:19 -0300

    ADD SetPetInMachine

--- src/classes/exercises/petmachine/MenuOptions.java
+++ src/classes/exercises/petmachine/MenuOptions.java
@@ -1,4 +1,24 @@
 package classes.exercises.petmachine;
 
+import java.util.Scanner;
+
 public class MenuOptions {
+
+    public final static Scanner scanner = new Scanner(System.in);
+    public static PetMachine petMachine = new PetMachine();
+
+    public static void giveShower(){
+        System.out.println("Give a shower ");
+    }
+    public static void setPetInMachine(){
+        var name = "";
+        while (name == null || name.isEmpty()){
+            System.out.println("Type Pet name: ");
+            name = scanner.nextLine();
+        }
+        var pet = new Pet(name);
+        petMachine.setPet(pet);
+        System.out.println("The pet "+ pet.getName()+" was placed in the machine");
+    }
+
 }
